8b14c5757ad54d38eebf4e0e2d53a3a78469431b
Isaac Robert <53939992+isaacrobert33@users.noreply.github.com>
2022-06-02 14:44:11 +0100

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,201 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from flask import request, jsonify
+from auth_middleware import token_required
+from models import User, Templates
+import os, sys, json
+
+
+app = Flask(__name__)
+api = Api(app)
+os.environ['SECRET_KEY'] = 'ayowumi33'
+SECRET_KEY = os.environ['SECRET_KEY']
+app.config['SECRET_KEY'] = SECRET_KEY
+
+def validate_email_and_password(email, password):
+    if "@" not in email and "." not in email:
+        return {"email": "invalid"}
+    if len(password) < 6:
+        return {"password": "invalid"}
+    return True
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    try:
+        print(request.method)
+        try:
+            data = request.json
+            print(data)
+        except Exception as e:
+            return jsonify({'message': 'error', 'error': str(e)}), 500
+        
+        if not data:
+            return jsonify({
+                'message': 'Please provide user details', 
+                'data': None,
+                'error': 'bad request'}), 400
+                
+        is_valid = validate_email_and_password(data["email"], data["password"])
+        
+        if type(is_valid) == dict:
+
+            return jsonify({
+                "message": "Invalid sign-up details",
+                "data": None,
+                "error": is_vaild}
+                ), 400
+            
+        new_user = User().register(data)
+        if not new_user:
+            return jsonify(
+                {
+                'message': 'User already exists!',
+                'data': None,
+                'error': 'bad request'
+                }), 400
+        return jsonify(
+                {'message': 'Registered Successfully!', 'data': new_user}
+            ), 201
+    except Exception as e:
+        return jsonify(
+            {
+            'message': 'Internal server error!',
+            'data': None,
+            'error': str(e)
+            }), 500
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    try:
+        credentials = request.json
+        if not credentials:
+            return jsonify({
+                'message': 'Please provide user details', 
+                'data': None,
+                'error': 'bad request'}), 400
+        logged = User().login(
+            credentials["email"],
+            credentials["password"])
+        
+        if not logged:
+            return jsonify({"message": "Invalid login!"}), 401
+        
+        return jsonify({"message": "Successfully fetched auth token!", "data": logged})
+    except Exception as e:
+        return jsonify(
+            {
+            'message': 'Internal server error!',
+            'data': None,
+            'error': str(e)
+            }), 500
+
+@app.route('/template/<string:template_id>',  methods=['POST', 'GET', 'PUT', 'DELETE'])
+@token_required
+def template(template_id):
+    if request.method == 'GET':
+        try:
+            if template_id == 'all':
+                # Get all templates
+                templates = Templates().get_all()
+                return jsonify(
+                    {
+                    "message": 'Retieved all templates successfully', 
+                    'data': templates}
+                    ), 201
+            else:
+                # Get a single template
+                template = Templates().get_by_id(template_id)
+                if not template:
+                    return jsonify(
+                        {
+                        "message": "Template does not exists!",
+                        "data": None,
+                        "error": "bad request"
+                        }), 400
+                        
+                return jsonify(
+                    {'message': 'Template retrieved!', 'data': template})
+        except Exception as e:
+            return jsonify(
+                {
+                'message': 'Internal server error!',
+                'data': None,
+                'error': str(e)
+                }), 500
+    elif request.method == 'POST':
+        try:
+            data = request.json
+            new_template = Templates().create_new(data)
+            if not new_template:
+                return jsonify(
+                    {
+                    "message": "Invalid template format!",
+                    "data": None,
+                    "error": "bad request"
+                    }), 400
+
+            return jsonify({'message': "Successfully created new template!", 'data': new_template})
+        except Exception as e:
+            return jsonify(
+                {
+                'message': 'Internal server error!',
+                'data': None,
+                'error': str(e)
+                }), 500
+    elif request.method == 'PUT':
+        try:
+            old_template = Templates().update(data)
+            if not old_template:
+                return jsonify(
+                    {
+                        "message": "Template does not exists!",
+                        "data": None,
+                        "error": "Not found"
+                    }), 404
+
+            return jsonify(
+                {
+                    "message": "Successfully updated template!",
+                    "data": old_template
+                }), 201
+
+        except Exception as e:
+            return jsonify(
+                {
+                'message': 'Internal server error!',
+                'data': None,
+                'error': str(e)
+                }), 500
+    elif request.method == 'DELETE':
+        try:
+            template = Templates().delete(template_id)
+            if not template:
+                return jsonify(
+                    {
+                        "message": "Template does not exists!",
+                        "data": None,
+                        "error": "Not found"
+                    }), 404
+
+            return jsonify(
+                {
+                    "message": "Successfully deleted template!",
+                    "data": template
+                }), 201
+        except Exception as e:
+            return jsonify(
+                {
+                'message': 'Internal server error!',
+                'data': None,
+                'error': str(e)
+                }), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+r = get("http://127.0.0.1:5000/register", headers=headers, data={'first_name': 'Isaac','last_name': 'Robert', 'email': 'isaac@gmail.com','password': '123456'})
+
+
+
---
+++ models.py
@@ -0,0 +1,135 @@
+from pymongo import MongoClient
+from flask import current_app as app
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import os
+import sys, bson
+
+
+try:
+    import dns
+except ImportError:
+    print(os.system("pip install dnspython==2.2.1"))
+    import dns
+client = MongoClient("mongodb+srv://isaacrobert:ayowumi33@cluster0.z1j5f.mongodb.net/?retryWrites=true&w=majority")
+
+db = client.db
+print("[*] connection to mongodb established successfully...")
+
+
+
+def validate_template(data):
+    kwd = ["template_name", "subject", "body"]
+    for i in kwd:
+        if i not in data.keys():
+            return False
+
+    return True
+
+class Templates:
+    def __init__(self):
+        pass
+
+    def create_new(self, template_data):
+        is_valid = validate_template(template_data)
+        if not is_valid:
+            return
+        new_template = db.templates.insert_one(template_data)
+        return self.get_by_id(new_template.inserted_id)
+
+    def get_all(self):
+        templates = db.templates.find({})
+        return [{**template, "_id": str(template['_id'])} for template in templates]
+
+    def get_by_id(self, template_id):
+        template = db.templates.find_one({'_id': bson.ObjectId(template_id)})
+        if not template:
+            return
+        template['_id'] = str(template['_id'])
+        return template
+
+    def get_by_name(self, template_name):
+        template = db.templates.find_one({'template_name': template_name})
+        template['_id'] = str(template['_id'])
+        return template
+
+    def update(self, data):
+        if not self.get_by_name(data['template_name']):
+            return
+        template = db.templates.update_one(
+            {'template_name': data['template_name']},
+            {'$set': data}
+        )
+        return self.get_by_name(data['template_name'])
+
+    def delete(self, template_id):
+        if not self.get_by_id(template_id):
+            return
+        template = db.templates.delete_one({'_id': bson.ObjectId(template_id)})
+        return template
+
+
+
+
+class User:
+    def __init__(self) -> None:
+        self.current_users = []
+
+    def register(self, data: dict):
+        if self.get_by_email(data["email"]):
+            return
+
+        data["active"] = True
+        data["password"] = self.encrypt_password(data["password"])
+        new_user = db.users.insert_one(data)
+        self.set_account_status(new_user.inserted_id, True)
+        return self.get_by_id(new_user.inserted_id).pop("password")
+
+    def login(self, email: str, password: str):
+        user = self.get_by_email(email)
+        if not user or not check_password_hash(user["password"], password):
+            return
+        user.pop("password")
+        user["token"] = jwt.encode(
+                    {"user_id": user["_id"]},
+                    app.config['SECRET_KEY'],
+                    algorithm="HS256"
+                    ).decode('utf-8')
+        self.set_account_status(user['_id'], True)
+
+        return user
+
+    def logout(self, email):
+        user = self.get_by_email(email)
+        if not user:
+            return
+        self.set_account_status(str(user['_id']), False)
+        return self.get_by_email(email).pop("password")
+
+    def set_account_status(self, user_id, status):
+        user = db.users.update_one(
+            {"_id": bson.ObjectId(user_id)},
+            {"$set": {"active": status}}
+            )
+        return self.get_by_id(user_id).pop("password")
+
+    def encrypt_password(self, password: str):
+        return generate_password_hash(password)
+
+    def get_by_email(self, email: str):
+        user = db.users.find_one({'email': email})
+        if not user:
+            return
+        user['_id'] = str(user['_id'])
+        return user
+
+    def get_by_id(self, user_id):
+        user = db.users.find_one({'_id': bson.ObjectId(user_id)})
+        if not user:
+            return
+        user['_id'] = str(user['_id'])
+        return user
+
+
+
+
